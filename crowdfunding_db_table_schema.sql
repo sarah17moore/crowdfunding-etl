161c47547911b615233968836dd45b9a78c1e9b9
sarah17moore <you@example.com>
2022-10-31 11:03:41 -0700

    Upload updated file

--- crowdfunding_db_table_schema.sql
+++ crowdfunding_db_table_schema.sql
@@ -23,6 +23,16 @@ CREATE TABLE "campaign" (
      )
 );
 
+CREATE TABLE "backers" (
+    "backer_id" varchar(10) NOT NULL,
+    "cf_id" int NOT NULL,
+    "first_name" varchar(50) NOT NULL,
+    "last_name" varchar(50) NOT NULL,
+    "email" varchar(100) NOT NULL,
+    CONSTRAINT "pk_backers" PRIMARY KEY (
+        "backer_id"
+    )
+)
 CREATE TABLE "category" (
     "category_id" varchar(10)   NOT NULL,
     "category_name" varchar(50)   NOT NULL,
@@ -59,3 +69,5 @@ REFERENCES "category" ("category_id");
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "subcategory" ("subcategory_id");
 
+ALTER TABLE "backers" ADD CONSTRAINT "fk_campaign_cf_id" FOREIGN KEY("cf_id")
+REFERENCES "campaign" ("cf_id");
